951b0282838f6017ec7a9063dc18c9ea50b533ba
ZuchiSpeed <zuchi737@gmail.com>
2024-04-20 20:30:33 +0200

    tested regis database endpoint

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 
 
 app = FastAPI()
@@ -18,3 +20,26 @@ redis = get_redis_connection(
     password="",
     decode_responses=True
 )
+
+class Delivery(HashModel):
+    budget: int = 0
+    notes: str = ""
+    
+    class Meta:
+        database = redis
+        
+class Event(HashModel):
+    delivery_id: str = ""
+    type: str
+    data: str
+    
+    class Meta:
+        database = redis
+        
+@app.post('/deliveries/create')
+async def create(request: Request):
+    body = await request.json()
+    delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
+    event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
+    return event
+    
